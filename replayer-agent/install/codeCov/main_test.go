79f95d333ca6d213f3ee966ede189a923a722b75
openex27 <openexkevin@gmail.com>
2020-04-24 14:44:10 +0800

    Improved unit testing

--- replayer-agent/install/codeCov/main_test.go
+++ replayer-agent/install/codeCov/main_test.go
@@ -7,9 +7,12 @@ import (
 	"sync"
 	"syscall"
 	"testing"
+	"time"
 
 	_ "github.com/didi/sharingan"
 )
+
+const waitFlagParseTime = 10
 
 var systemTest *bool
 var endRunning chan bool
@@ -33,12 +36,30 @@ func init() {
 	systemTest = flag.Bool("systemTest", false, "Set to true when running system tests")
 }
 
-// Test started when the test binary is started. Only calls main.
-func Test_main(t *testing.T) {
+// TestMain Test started when the test binary is started. Only calls main.
+func TestMain(m *testing.M) {
 	if *systemTest {
+		go main()
 		signalHandler()
 		endRunning = make(chan bool, 1)
-		go main()
+		// Maximum waiting time(10s) for flag.Parse.
+		// If the flag still missed to execute after 10 seconds, check your logic with main function.
+		checkTime := time.After(waitFlagParseTime * time.Second)
+		for {
+			if flag.Parsed() {
+				break
+			}
+			select {
+			case <-checkTime:
+				if !flag.Parsed() {
+					flag.Parse()
+				}
+				break
+			default:
+				time.Sleep(200 * time.Millisecond)
+			}
+		}
 		<-endRunning
+		os.Exit(m.Run())
 	}
 }
