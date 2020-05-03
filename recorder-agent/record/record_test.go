362273cad40b8c95411df74a4cb705c5c185aa72
yufeng <yufeng@didichuxing.com>
2020-05-03 16:17:11 +0800

    update recorder-agent

--- recorder-agent/record/record_test.go
+++ recorder-agent/record/record_test.go
@@ -13,7 +13,7 @@ func TestFliter(t *testing.T) {
 	str = `{"operate":100001,"data":{"Context":"hostname ","ThreadId":2612231,"SessionId":"1548745599676096093-2612231","TraceHeader":null,"NextSessionId":"1548745599676604413-2612231","CallFromInbound":{"ActionIndex":0,"OccurredAt":1548745599676102064,"ActionType":"CallFromInbound","Peer":{"IP":"127.0.0.1","Port":53442,"Zone":""},"UnixAddr":{"Name":"","Net":""},"Request":"POST /report/set HTTP/1.0\r\nHost: 127.0.0.1:9999\r\nConnection: close\r\nContent-Length: 618\r\nxxx-header-traceid: 645a693a5c4ffb7fae8a4c1f14637602\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nappid=2\u0026action=1"},"ReturnInbound":{"ActionIndex":0,"OccurredAt":1548745599676544387,"ActionType":"ReturnInbound","Response":"HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nDate: Tue, 29 Jan 2019 07:06:39 GMT\r\nContent-Length: 25\r\n\r\n{\"errno\":0,\"errmsg\":\"ok\"}"},"Actions":[{"ActionIndex":0,"OccurredAt":1548745599676544387,"ActionType":"ReturnInbound","Response":"HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nDate: Tue, 29 Jan 2019 07:06:39 GMT\r\nContent-Length: 25\r\n\r\n{\"errno\":0,\"errmsg\":\"ok\"}"}],"TraceId":"645a693a5c4ffb7fae8a4c1f14637602","SpanId":""}}`
 
 	// 0.5r/s，2秒一个请求
-	bucketMap.m["/index.php"] = ratelimit.NewBucketWithRate(0.5, defaultBucketCapacity)
+	bucketMap.m["/report/set"] = ratelimit.NewBucketWithRate(0.5, defaultBucketCapacity)
 
 	var res bool
 
